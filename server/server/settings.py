cf2848a3de3c3ce5ee6bd91a81dbe1ef2ed5119e
mishrakeshav <47028747+mishrakeshav@users.noreply.github.com>
2021-10-02 08:04:49 +0530

    update backend models and restore data

--- server/server/settings.py
+++ server/server/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'recommendation.apps.RecommendationConfig',
     'django_filters',
+      'corsheaders',
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -85,6 +87,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
 
 
 # Password validation
